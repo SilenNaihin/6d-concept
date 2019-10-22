106902a3261a6e1bb02476bb29f5d4f43cb67a75
Silen Naihin <silen.naihin@gmail.com>
2019-10-22 11:53:27 -0400

    Initializing Repository

---
+++ 3. Dashboard/Main.js
@@ -0,0 +1,6 @@
+// document.getElementByClass('profilepicture')
+//     if the database returns girl
+//         set profile image to girl and vice versa
+
+// const upcomingarrow = document.getElementById('upcomingarrow');
+
